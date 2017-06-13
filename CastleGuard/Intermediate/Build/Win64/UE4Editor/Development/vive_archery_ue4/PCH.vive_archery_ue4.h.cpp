c8a8b51ddea2a4f3e92020f4aaf2716b7fccf8b8
lk <il>
2017-06-12 22:59:34 -0700

    Hopefully this will preserve functionality

---
+++ CastleGuard/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/PCH.vive_archery_ue4.h.cpp
@@ -0,0 +1,1 @@
+#include "E:\CastleGuardLoneDefender\Castle-Guard-Working-Bow\CastleGuard\ProjectFiles\Source\vive_archery_ue4\vive_archery_ue4.h"
---
+++ CastleGuard/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/vive_archery_ue4.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_VIVE_ARCHERY_UE4_H__
+#define __AUTO_VIVE_ARCHERY_UE4_H__
+//Last Write: 6/11/2017 5:23:48 AM +00:00
+#include "E:\CastleGuardLoneDefender\Castle-Guard-Working-Bow\CastleGuard\ProjectFiles\Source\vive_archery_ue4\vive_archery_ue4.h"
+#endif//__AUTO_VIVE_ARCHERY_UE4_H__
